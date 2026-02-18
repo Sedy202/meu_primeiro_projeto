b440b2244bec03e05dcb88b7e67bf96c7603afe0
Sedy202 <actup202@gmail.com>
2026-02-18 16:22:37 -0300

    Add modern slang dictionary in Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo vergonhoso ou constrangedor",
+            "STALKEAR": "Investigar a vida de alguém online",
+            "SKIBIDI": "Um grupo de videos do youtube shorts em que cabeças em privadas attacam o mundo",
+            "MOGGADO": "Algo ou alguem que foi supperado ou derrotado",
+            }
+meme_dict["cringe"] = "Algo vergonhoso ou constrangedor"
+
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    print(word)
+else:
+    print("sla q isso paizão kkkkkkkkkkkkkkkkkkkkkkkkk")
